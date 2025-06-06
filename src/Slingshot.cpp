69e9aeb983a53958273c66f72225689a179d2cbf
garyhung0584 <garyhung0584@gmail.com>
2025-06-06 15:27:35 +0800

    feat small wire

--- src/Slingshot.cpp
+++ src/Slingshot.cpp
@@ -6,8 +6,10 @@ Slingshot::Slingshot(const glm::vec2 &pos) {
     const auto m_Slingshot0 = std::make_shared<Util::GameObject>(std::make_shared<Util::Image>(RESOURCE_DIR"/Slingshot/Slingshot.png"), 5);
     const auto m_slingshot1 = std::make_shared<Util::GameObject>(std::make_shared<Util::Image>(RESOURCE_DIR"/Slingshot/Slingshot1.png"), 0);
     const auto m_wire = std::make_shared<Util::GameObject>(std::make_shared<Util::Image>(RESOURCE_DIR"/Slingshot/wire.png"), 0);
-    for (const auto &wire: m_wires) {
-        wire->SetVisible(false);
+    for (int i; i < 2; i++) {
+        const auto m_wire = std::make_shared<Util::GameObject>(std::make_shared<Util::Image>(RESOURCE_DIR"/Slingshot/wireSmall.png"), 0);
+        m_wire->SetVisible(false);
+        m_wires.push_back(m_wire);
     }
     m_slingshot.push_back(m_Slingshot0);
     m_slingshot.push_back(m_slingshot1);
